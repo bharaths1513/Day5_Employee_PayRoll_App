6ae05c2530919dec2a9ddc3d167e1dad8a093ecb
bharaths1513 <bharaths.1311@gmail.com>
2021-12-23 17:51:47 +0530

    added: UC1 Created Employee PayRoll Project

---
+++ src/main/java/com/example/Demo/Day4EmployeePayRollAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day4EmployeePayRollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day4EmployeePayRollAppApplication.class, args);
+	}
+
+}
